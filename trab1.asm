5bb05716ce81ff59f5825e65647710d45d2dbec5
MarceloMontanher <marcelo.montanher14@gmail.com>
2015-05-11 20:59:06 -0300

    correcao na verificacao de entrada

--- trab1.asm
+++ trab1.asm
@@ -42,10 +42,10 @@ main:
 	syscall
 	
 	# Le numero digitado pelo usuario como string
-	li $v0, 8 			# take in input
-	la $a0, STR_INPUT 	# load byte space into address
-	li $a1, 64 			# allot the byte space for string
-	move $s2, $a0 		# armazena a string em s2
+	li 	$v0, 8 			# take in input
+	la 	$a0, STR_INPUT 		# load byte space into address
+	li 	$a1, 64 		# allot the byte space for string
+	move 	$s2, $a0 		# armazena a string em s2
 	syscall
 	
 	# Print ("O numero deve ser convertido para qual base? (B)Binario (O)Octal (D)Decimal (H)Hexadecimal")
@@ -125,8 +125,8 @@ baseInicialNaoEhDecimal:
 	j	encerraPrograma
 	
 imprimeResultado:
-	li $v0, 4
-	move $a0, $s3
+	li 	$v0, 4
+	move 	$a0, $s3
 	syscall
 	
 	# Encerra programa
@@ -137,81 +137,81 @@ encerraPrograma:
 #------------- Definicao de fluxo do programa -------------#
 # Binario
 BinToOct:
-	li $a0, 2
-	jal funcao_stringToNumber
-	li $a0, 8
+	li 	$a0, 2
+	jal 	funcao_stringToNumber
+	li 	$a0, 8
 	jal	funcao_numberToString
 	j	imprimeResultado
 BinToDec:
-	li $a0, 2
-	jal funcao_stringToNumber
-	li $a0, 10
+	li 	$a0, 2
+	jal 	funcao_stringToNumber
+	li 	$a0, 10
 	jal	funcao_numberToString
 	j	imprimeResultado
 BinToHex:
-	li $a0, 2
-	jal funcao_stringToNumber
-	li $a0, 16
+	li 	$a0, 2
+	jal 	funcao_stringToNumber
+	li 	$a0, 16
 	jal	funcao_numberToString
 	j	imprimeResultado
 
 # Octal
 OctToBin:
-	li $a0, 8
-	jal funcao_stringToNumber
-	li $a0, 2
+	li 	$a0, 8
+	jal 	funcao_stringToNumber
+	li 	$a0, 2
 	jal	funcao_numberToString
 	j	imprimeResultado
 OctToDec:
-	li $a0, 8
-	jal funcao_stringToNumber
-	li $a0, 10
+	li 	$a0, 8
+	jal 	funcao_stringToNumber
+	li 	$a0, 10
 	jal	funcao_numberToString
 	j	imprimeResultado
 OctToHex:
-	li $a0, 8
-	jal funcao_stringToNumber
-	li $a0, 16
+	li 	$a0, 8
+	jal 	funcao_stringToNumber
+	li 	$a0, 16
 	jal	funcao_numberToString
 	j	imprimeResultado
 
 # Decimal	
 DecToBin:
-	li $a0, 10
-	jal funcao_stringToNumber
-	li $a0, 2
+	li 	$a0, 10
+	jal 	funcao_stringToNumber
+	li 	$a0, 2
 	jal	funcao_numberToString
 	j	imprimeResultado
 DecToOct:
-	li $a0, 10
-	jal funcao_stringToNumber
-	li $a0, 8
+	li	$a0, 10
+	jal 	funcao_stringToNumber
+	li 	$a0, 8
 	jal	funcao_numberToString
 	j	imprimeResultado
 DecToHex:
-	li $a0, 10
-	jal funcao_stringToNumber
-	li $a0, 16
+	li 	$a0, 10
+	jal 	funcao_stringToNumber
+	li 	$a0, 16
 	jal	funcao_numberToString
 	j	imprimeResultado
 
 # Hexadecimal
 HexToBin:
-	li $a0, 16
-	jal funcao_stringToNumber
-	li $a0, 2
+	li 	$a0, 16
+	jal 	funcao_stringToNumber
+	li 	$a0, 2
 	jal	funcao_numberToString
 	j	imprimeResultado
 HexToOct:
-	li $a0, 16
-	jal funcao_stringToNumber
-	li $a0, 8
+	li 	$a0, 16
+	jal 	funcao_stringToNumber
+	li	$a0, 8
 	jal	funcao_numberToString
 	j	imprimeResultado
 HexToDec:
-	li $a0, 16
-	jal funcao_stringToNumber
-	li $a0, 10
+	li 	$a0, 16
+	jal 	funcao_stringToNumber
+	li 	$a0, 10
 	jal	funcao_numberToString
 	j	imprimeResultado
 
@@ -219,146 +219,146 @@ HexToDec:
 #****************************************** Funcoes de Conversao ******************************************#
 
 #-----------------------------------------------------------------------#
-#	Converte numero para string 										#
-#	Entrada $a0 = tipo da base destino									#
-#	Grava $s2 = numero													#
+#	Converte numero para string 					#
+#	Entrada $a0 = tipo da base destino				#
+#	Grava $s2 = numero						#
 #-----------------------------------------------------------------------#
 
 funcao_numberToString:
 	# Save our return pointer
-	addi $sp, $sp, -4
-	sw $ra, 4($sp)
+	addi 	$sp, $sp, -4
+	sw 	$ra, 4($sp)
 
 	# Run all 32 bits of our number width = z
-	li $t0, 31
-	li $t7, 2
-	beq $a0, $t7, funcao_numberToString_init
+	li 	$t0, 31
+	li 	$t7, 2
+	beq 	$a0, $t7, funcao_numberToString_init
 
-	li $t0, 9
-	li $t7, 8
-	beq $a0, $t7, funcao_numberToString_init
+	li 	$t0, 9
+	li 	$t7, 8
+	beq 	$a0, $t7, funcao_numberToString_init
 
-	li $t0, 8
-	li $t7, 10
-	beq $a0, $t7, funcao_numberToString_init
+	li 	$t0, 8
+	li 	$t7, 10
+	beq 	$a0, $t7, funcao_numberToString_init
 
-	li $t0, 7
-	li $t7, 16
-	beq $a0, $t7, funcao_numberToString_init
+	li 	$t0, 7
+	li 	$t7, 16
+	beq 	$a0, $t7, funcao_numberToString_init
 
 funcao_numberToString_init:
-	move $t1, $s2		# Move our number to our temporary register
-	li $t5, 0			# Define our return as zero for now
-	move $t6, $a0		# Move our exponent base to a safe place
-	li $t7, 0 			# Reset our inner loop register
+	move 	$t1, $s2		# Move our number to our temporary register
+	li 	$t5, 0			# Define our return as zero for now
+	move 	$t6, $a0		# Move our exponent base to a safe place
+	li 	$t7, 0 			# Reset our inner loop register
 
 funcao_numberToString_loop:
-	move $t7, $t6 		# Define our inner loop register
-	subi $t7, $t7, 1 	# Decrement to address pointer default logic (start at zero)
+	move 	$t7, $t6 		# Define our inner loop register
+	subi 	$t7, $t7, 1 		# Decrement to address pointer default logic (start at zero)
 
 funcao_numberToString_innerLoop:
-	move $a0, $t6		# Define our base to be exponentiated
-	move $a1, $t0		# Move our main argument to our exponential function
-	jal exponential
+	move 	$a0, $t6		# Define our base to be exponentiated
+	move 	$a1, $t0		# Move our main argument to our exponential function
+	jal 	exponential
 
-	move $t2, $v0		# Move our result to our temporary register
-	mul $t2, $t2, $t7 	# w = (base - alpha) * (base ^ (z))
+	move 	$t2, $v0		# Move our result to our temporary register
+	mul 	$t2, $t2, $t7 		# w = (base - alpha) * (base ^ (z))
 
-	blt $t1, $t2, funcao_numberToString_innerContinue
-	blt $t2, $zero, funcao_numberToString_innerContinue
+	blt 	$t1, $t2, funcao_numberToString_innerContinue
+	blt 	$t2, $zero, funcao_numberToString_innerContinue
 
-	sub $t3, $t1, $t2	# Get the result from (y - w)
-	move $t1, $t3		# Reduce our number for the next interaction
+	sub 	$t3, $t1, $t2		# Get the result from (y - w)
+	move 	$t1, $t3		# Reduce our number for the next interaction
 
 	# Concatenate strings
-	li $a1, '0'
-	li $t4, 0
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '0'
+	li 	$t4, 0
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '1'
-	li $t4, 1
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '1'
+	li 	$t4, 1
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '2'
-	li $t4, 2
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '2'
+	li 	$t4, 2
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '3'
-	li $t4, 3
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '3'
+	li 	$t4, 3
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '4'
-	li $t4, 4
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '4'
+	li 	$t4, 4
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '5'
-	li $t4, 5
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '5'
+	li 	$t4, 5
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '6'
-	li $t4, 6
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '6'
+	li 	$t4, 6
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '7'
-	li $t4, 7
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '7'
+	li 	$t4, 7
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '8'
-	li $t4, 8
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '8'
+	li 	$t4, 8
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, '9'
-	li $t4, 9
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, '9'
+	li 	$t4, 9
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'A'
-	li $t4, 10
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li	$a1, 'A'
+	li 	$t4, 10
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'B'
-	li $t4, 11
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, 'B'
+	li 	$t4, 11
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'C'
-	li $t4, 12
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, 'C'
+	li 	$t4, 12
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'D'
-	li $t4, 13
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, 'D'
+	li 	$t4, 13
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'E'
-	li $t4, 14
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, 'E'
+	li 	$t4, 14
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
-	li $a1, 'F'
-	li $t4, 15
-	beq $t4, $t7, funcao_numberToString_innerLoopConcatenate
+	li 	$a1, 'F'
+	li 	$t4, 15
+	beq 	$t4, $t7, funcao_numberToString_innerLoopConcatenate
 
 funcao_numberToString_innerLoopConcatenate:
 	# move $a0, $s3		# Our origin string
 	#jal strcopier
 
-	li $v0, 11
-	move $a0, $a1
+	li 	$v0, 11
+	move 	$a0, $a1
 	syscall
 
 	j funcao_numberToString_continue
 
 funcao_numberToString_innerContinue:
-	subi $t7, $t7, 1 	# Reduce (base - alpha) until we get to zero
+	subi 	$t7, $t7, 1 	# Reduce (base - alpha) until we get to zero
 
-	bge $t7, $zero, funcao_numberToString_innerLoop
+	bge 	$t7, $zero, funcao_numberToString_innerLoop
 
 funcao_numberToString_continue:
-	subi $t0, $t0, 1   	# Reduce our base until we got zero
+	subi 	$t0, $t0, 1   	# Reduce our base until we got zero
 
-	bge $t0, $zero, funcao_numberToString_loop
+	bge 	$t0, $zero, funcao_numberToString_loop
 
 funcao_numberToString_end:
 	# Restore our return pointer
 	lw	$ra, 4($sp)
-	addi $sp, $sp, 4
+	addi 	$sp, $sp, 4
 
 	jr	$ra
 
@@ -370,108 +370,108 @@ funcao_numberToString_end:
 
 funcao_stringToNumber:
 	# Save our return pointer
-	addi $sp, $sp, -4
-	sw $ra, 4($sp)
+	addi 	$sp, $sp, -4
+	sw 	$ra, 4($sp)
 
 	# Save our base size
-	move $t6, $a0
+	move 	$t6, $a0
 
 	# Get string length
-	move $a0, $s2
-	jal strlen
-	move $t3, $v0
+	move 	$a0, $s2
+	jal 	strlen
+	move 	$t3, $v0
 
 	# Inicia o retorno
-	move $t4, $s2
-	li $t5, 0
+	move 	$t4, $s2
+	li 	$t5, 0
 
 funcao_stringToNumber_loop:
-	lb $t2, 0($t4)   							# load the next character to t2
-	beq $t2, $zero, funcao_stringToNumber_end 	# end loop if null character is reached
+	lb 	$t2, 0($t4)   							# load the next character to t2
+	beq 	$t2, $zero, funcao_stringToNumber_end 	# end loop if null character is reached
 
-	li $t0, '0'
-	li $t1, 0
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '0'
+	li 	$t1, 0
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '1'
-	li $t1, 1
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '1'
+	li 	$t1, 1
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '2'
-	li $t1, 2
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '2'
+	li 	$t1, 2
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '3'
-	li $t1, 3
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '3'
+	li 	$t1, 3
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '4'
-	li $t1, 4
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '4'
+	li 	$t1, 4
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '5'
-	li $t1, 5
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '5'
+	li 	$t1, 5
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '6'
-	li $t1, 6
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '6'
+	li 	$t1, 6
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '7'
-	li $t1, 7
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '7'
+	li 	$t1, 7
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '8'
-	li $t1, 8
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '8'
+	li 	$t1, 8
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, '9'
-	li $t1, 9
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, '9'
+	li 	$t1, 9
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'A'
-	li $t1, 10
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li	$t0, 'A'
+	li 	$t1, 10
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'B'
-	li $t1, 11
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, 'B'
+	li 	$t1, 11
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'C'
-	li $t1, 12
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, 'C'
+	li 	$t1, 12
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'D'
-	li $t1, 13
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, 'D'
+	li 	$t1, 13
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'E'
-	li $t1, 14
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, 'E'
+	li 	$t1, 14
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
-	li $t0, 'F'
-	li $t1, 15
-	beq $t0, $t2, funcao_stringToNumber_continue
+	li 	$t0, 'F'
+	li 	$t1, 15
+	beq 	$t0, $t2, funcao_stringToNumber_continue
 
 funcao_stringToNumber_continue:
-	move $a0, $t6		# Define our base to be exponentiated
-	move $a1, $t3		# Move our main argument to our exponential function
-	subi $a1, $a1, 1 	# Bases exponenciais comecam a contar em 0
-	jal exponential
+	move 	$a0, $t6		# Define our base to be exponentiated
+	move 	$a1, $t3		# Move our main argument to our exponential function
+	subi 	$a1, $a1, 1 	# Bases exponenciais comecam a contar em 0
+	jal	exponential
 
-	mul $t1, $t1, $v0 	# Multiplica as bases
-	subi $t3, $t3, 1 	# Decrementa o tamanho da string
-	add $t5, $t5, $t1 	# Soma o resultado a base
-	addi $t4, $t4, 1 	# Aumenta a posicao do nosso registrador de caracter
+	mul	$t1, $t1, $v0 	# Multiplica as bases
+	subi 	$t3, $t3, 1 	# Decrementa o tamanho da string
+	add 	$t5, $t5, $t1 	# Soma o resultado a base
+	addi 	$t4, $t4, 1 	# Aumenta a posicao do nosso registrador de caracter
 
-	bne $t3, $zero, funcao_stringToNumber_loop
+	bne 	$t3, $zero, funcao_stringToNumber_loop
 
 funcao_stringToNumber_end:
-	move $s2, $t5		# Move to our end pointer
+	move 	$s2, $t5		# Move to our end pointer
 
 	# Restore our return pointer
 	lw	$ra, 4($sp)
-	addi $sp, $sp, 4
+	addi 	$sp, $sp, 4
 
 	jr	$ra
 	
@@ -483,8 +483,6 @@ funcao_stringToNumber_end:
 #-----------------------------------------------------------------------#
 verificaBase:
 	# Empilha registradores da funcao
-	
-	li	$v0, 0		# inicia retorno como falso
 	
 	li	$t0, 'B'	# compara com B
 	beq	$t0, $a0, verificaBaseConfirma
@@ -504,6 +502,8 @@ verificaBase:
 	la	$a0, STR_ENTRADA_INVALIDA
 	syscall
 	
+	li	$v0, 0		# retorna como falso
+	
 	j	verificaBaseFim
 	
 verificaBaseConfirma:
@@ -513,28 +513,28 @@ verificaBaseFim:
 	jr	$ra
 
 #-----------------------------------------------------------------------#
-#	Realiza a exponeciacao do numero										#
-#	Entrada $a0 = base; $a1 = expoente 									#
-#	Retorna $v0 = 1, se tudo estiver ok									#
+#	Realiza a exponeciacao do numero				#
+#	Entrada $a0 = base; $a1 = expoente 				#
+#	Retorna $v0 = 1, se tudo estiver ok				#
 #-----------------------------------------------------------------------#
 exponential:
-	addi $sp, $sp, -4
-	sw $t0, 4($sp)
-	move $t0, $zero
-	li $v0, 1
+	addi 	$sp, $sp, -4
+	sw 	$t0, 4($sp)
+	move 	$t0, $zero
+	li 	$v0, 1
 exponential_loop: 
 	beq	$t0, $a1, exponential_end	# Checks to see if $t0 is equal to $a1 if not
 						# it continues, if it is it jumps to end
-	mul	$v0, $v0, $a0	# Multiplies the value in $a0 by the value in $v0
-	addi $t0, $t0, 1	# Adds 1 to $t0 and stores it in $t0 because
+	mul	$v0, $v0, $a0			# Multiplies the value in $a0 by the value in $v0
+	addi 	$t0, $t0, 1			# Adds 1 to $t0 and stores it in $t0 because
 						# $t0 is the loop counter
-	j exponential_loop	# Jumps to the beginning of the loop to start
+	j 	exponential_loop		# Jumps to the beginning of the loop to start
 						# the process over
 exponential_end:
 	# restore $t0 and the stack
 	lw	$t0, 4($sp)
-	addi $sp, $sp, 4
-	jr $ra
+	addi 	$sp, $sp, 4
+	jr 	$ra
 	
 	
 #-----------------------------------------------------------------------#
